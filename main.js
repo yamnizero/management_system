611d36016161ebbc46d040bafaa20cc0c4e7e665
mohammed jamal <yamni.zero@gmail.com>
2023-09-25 23:09:34 +0200

    done product

--- main.js
+++ main.js
@@ -36,19 +36,23 @@ if(localStorage.product != null){
 submit.onclick = function(){
     // object
     let newPro ={
-        title: title.value,
+        title: title.value.toLowerCase(),
         price: price.value,
         taxes : taxes.value,
         ads: ads.value,
         discount: discount.value,
         total:total.innerHTML,
         count: count.value,
-        category: category.value,
+        category: category.value.toLowerCase(),
     }
 
-    if(mood == 'create'){
-        if(newPro.count > 1){
-            for (let i = 0; i < newPro.count; i++) {
+    if(title.value != ''&& price.value != '' && category.value != '' && newPro.count < 100)
+    {
+        if(mood == 'create')
+        {
+            if(newPro.count > 1){
+            for (let i = 0; i < newPro.count; i++) 
+            {
                 dataPro.push(newPro);
             }
         }else{
@@ -60,11 +64,14 @@ submit.onclick = function(){
         submit.innerHTML = 'Create';
         count.style.display = 'block';
     }
+    clearData() 
+    }
+    
     
     
     // save localStorage
     localStorage.setItem('product' , JSON.stringify(dataPro))
-    clearData() 
+   
     showData()
 }
 
@@ -89,7 +96,7 @@ function clearData(){
         for (let i = 0; i < dataPro.length; i++) {
             table += `
             <tr>
-                <td>${i}</td>
+                <td>${i+1}</td>
                 <td>${dataPro[i].title}</td>
                 <td>${dataPro[i].price}</td>
                 <td>${dataPro[i].taxes}</td>
@@ -100,7 +107,7 @@ function clearData(){
                 <td><button onclick="updateData(${i})" id="update">update</button></td>
                 <td><button onclick="deleteData( ${i} )" id="delete">delete</button></td>
              </tr>
-            `
+            `;
         }
         document.getElementById('tbody').innerHTML = table;
         let btnDeleteAll = document.getElementById('deleteAll');
@@ -156,8 +163,67 @@ function  updateData(i)
 let searchMood = 'title';
 function getSearchMood(id)
 {
+    let search = document.getElementById('search');
+    if(id ==  'searchTitle'){
+        searchMood = 'title';
+    }else{
+        searchMood = 'category';  
+    }
+    search.placeholder = 'Search By '+ searchMood;
+    search.focus()
+    search.value = ''; 
+    showData()
+}
 
+function searchData(value)
+{
+    let table = '';
+    for (let i = 0; i < dataPro.length; i++)
+    {
+        if(searchMood == 'title')
+        {
+        if(dataPro[i].title.includes(value.toLowerCase()))
+        {
+            table += `
+                <tr>
+                <td>${i}</td>
+                <td>${dataPro[i].title}</td>
+                <td>${dataPro[i].price}</td>
+                <td>${dataPro[i].taxes}</td>
+                <td>${dataPro[i].ads}</td>
+                <td>${dataPro[i].discount}</td>
+                <td>${dataPro[i].total}</td>
+                <td>${dataPro[i].category}</td>
+                <td><button onclick="updateData(${i})" id="update">update</button></td>
+                <td><button onclick="deleteData( ${i} )" id="delete">delete</button></td>
+                </tr>
+            `;
+        }
+        }else{
+        
+            if(dataPro[i].category.includes(value)){
+                table += `
+                   <tr>
+                    <td>${i}</td>
+                    <td>${dataPro[i].title}</td>
+                    <td>${dataPro[i].price}</td>
+                    <td>${dataPro[i].taxes}</td>
+                    <td>${dataPro[i].ads}</td>
+                    <td>${dataPro[i].discount}</td>
+                    <td>${dataPro[i].total}</td>
+                    <td>${dataPro[i].category}</td>
+                    <td><button onclick="updateData(${i})" id="update">update</button></td>
+                    <td><button onclick="deleteData( ${i} )" id="delete">delete</button></td>
+                    </tr>
+                 `;
+            }
+       
+    }
+    }
+    document.getElementById('tbody').innerHTML = table;
 }
 
 // clean data
+
+
  
